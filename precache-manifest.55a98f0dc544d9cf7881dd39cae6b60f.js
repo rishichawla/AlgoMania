d3b4b9fb84c66729b1f4177cf7bf2d3d8de0a7fd
Arpit Agrawal <arpitagrawal3018@gmail.com>
2020-05-05 12:50:56 +0530

    Updates

---
+++ precache-manifest.55a98f0dc544d9cf7881dd39cae6b60f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed639be94f54c5779943ad8486df81f9",
+    "url": "/algomania/index.html"
+  },
+  {
+    "revision": "73de43367af07c094d16",
+    "url": "/algomania/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "74b0a9ac63c00f395a9b",
+    "url": "/algomania/static/js/2.b5819e98.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/algomania/static/js/2.b5819e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73de43367af07c094d16",
+    "url": "/algomania/static/js/main.5eff15b3.chunk.js"
+  },
+  {
+    "revision": "60d9de9ee2a5aa2eb61b",
+    "url": "/algomania/static/js/runtime-main.cc5dba8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/algomania/static/media/logo.5d5d9eef.svg"
+  }
+]);
